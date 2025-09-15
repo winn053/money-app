15e49e73e7d36d1ff90a9f9482fd885fae346121
winn053 <winnegar@yahoo.com>
2025-09-15 10:24:28 -0700

    added Reset button to delete all spending data

---
+++ delete-all-data.js
@@ -0,0 +1,11 @@
+const resetButton = document.getElementById('clear-data-button');
+
+function clearAll() {
+  if (confirm('This will delete ALL transactions. Continue?')) {
+    const data = [];
+    saveData(KEY, data);
+    clearSearchTable();
+  }
+}
+
+resetButton.addEventListener('click', clearAll);
